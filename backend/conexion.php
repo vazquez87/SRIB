68d47903b1f3ba74ce2b6f418011a90bd46e0f3f
Osscar009 <ty99yndhpj@privaterelay.appleid.com>
2026-06-30 11:14:13 -0600

    Add database connection setup in conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,1 +1,14 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "srib_db";
 
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8mb4");
+?>
